39b5b6be14f0c03a01b9b7aa0fd4ea9101bd21a6
Lukichi-ve <lucasve45@gmail.com>
2025-08-12 14:41:18 -0300

    solucion al clickear boton

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.querySelector('div');
+const boton = document.querySelector('button');
 
 div.addEventListener('click', function() {
     alert('Hola! Soy el div');
-})
+});
+
+boton.addEventListener('click', function(event){
+    alert('Hola!')
+    event.stopPropagation();
+});
